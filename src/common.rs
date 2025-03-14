4c1726b6e083bdd93d98908c71fa1578c7557538
DESKTOP-I93C10G\admin <rickychaw@outlook.com>
2025-03-14 12:55:20 +0800

    重新添加子模块

--- src/common.rs
+++ src/common.rs
@@ -934,7 +934,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://ddns.qiandao88.top:7000".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
