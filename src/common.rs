ea2a8e440a3f73e52ef80bc76fb3e7b465298454
rickychaw <rickychaw@outlook.com>
2025-10-24 23:32:59 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://ddns.qiandao88.top".to_owned()
+    "http://ddns.qiandao88.top:21114".to_owned()
 }
 
 #[inline]
